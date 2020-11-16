a1f8943bc5856a5974c8a41b9513c140bae71172
Manoel Vitor <manoelvitorka@gmail.com>
2020-11-16 14:19:22 +0000

    Finished Car Controller

---
+++ src/controllers/carController.js
@@ -0,0 +1,57 @@
+const boom = require("boom")
+
+const Car = require("../models/Car")
+
+// Get all Cars
+exports.getCars = async (req, res) => {
+    try {
+        const cars = await Car.find()
+        return cars
+    } catch (err) {
+        throw boom.boomify(err)
+    }
+}
+
+// Get Single Car by ID
+exports.getSingleCar = async (req, res) => {
+    try {
+        const id = req.params.id
+        const car = await Car.findById(id)
+        return car
+    } catch (err) {
+        throw boom.boomify(err)
+    }
+}
+
+// Add a new Car
+exports.addCar = async (req, res) => {
+    try {
+        const car = new Car(req.body)
+        return car.save()
+    } catch (err) {
+        throw boom.boomify(err)
+    }
+}
+
+// Update an existing Car
+exports.updateCar = async (req, res) => {
+    try {
+        const id = req.params.id
+        const car = req.body
+        const { ...updateData } = car
+        const update = await Car.findByIdAndUpdate(id, updateData, { new: true})
+        return
+    } catch (err) {
+        throw boom.boomify(err)
+    }
+}
+
+// Delete a Car
+exports.deleteCar = async (req, res) => {
+    try {
+        const id = req.params.id
+        const car = await Car.findByIdAndRemove(id)
+    } catch (err) {
+        throw boom.boomify(err)
+    }
+}
