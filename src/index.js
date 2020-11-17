852888fcb06fc8f0d12dc7d92719554682c21adc
Manoel Vitor <manoelvitorka@gmail.com>
2020-11-17 13:52:54 +0000

    Reorganising file structure

--- src/index.js
+++ src/index.js
@@ -1,39 +1,27 @@
-// Routes
+// Import Server
+const fastify = require("./server.js")
+
+// .ENV File
+require("dotenv").config()
+
+// Import Routes
 const routes = require("./routes")
 
-// Swagger Options
+// Import Swagger Options
 const swagger = require("./config/swagger")
-
-// Mongoose
-const mongoose = require("mongoose")
-
-// Env Configure
-const dotenv = require("dotenv")
-dotenv.config()
-
-// Require the framework
-const fastify = require("fastify")({
-    logger: true
-})
 
 // Register Swagger
 fastify.register(require("fastify-swagger"), swagger.options)
 
-// Declare the route
+// Loop over each route
 routes.forEach((route, index) => {
     fastify.route(route)
 })
 
-
-// Connect to MongoDB
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphql-api.fyhgt.mongodb.net/fastifyapi?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log("MongoDB Connected..."))
-.catch(err => console.log(err))
-
-// Run the Server
+// Run the Server!
 const start = async () => {
     try {
-        await fastify.listen(3000)
+        await fastify.listen(process.env.PORT || 3000, '0.0.0.0')
         fastify.swagger()
         fastify.log.info(`Server Listening on ${fastify.server.address().port}`)
     } catch (err) {
