d80098fd19abf1346582b3c93efbcfcecb16cccc
Manoel Vitor <manoelvitorka@gmail.com>
2020-11-19 10:32:26 +0000

    Reestructuring folders

--- src/models/Owner.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const ownerSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String
-})
-
-module.exports = mongoose.model("Owner", ownerSchema)
